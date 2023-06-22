e7188b35a5192dfe48039baf8cf7928ec7f9e3db
cyber_octopus <gsy@gsy123mailbox.com>
2023-06-22 19:31:22 +0800

    add TouchPadPWM

---
+++ TouchPadPWM/HARDWARE/TIMER/timer.c
@@ -0,0 +1,60 @@
+#include "timer.h"
+#include "led.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用
+//ALIENTEK STM32F7开发板
+//定时器中断驱动代码	   
+//Copyright(C) 广州市星翼电子科技有限公司 2014-2024
+//All rights reserved									  
+////////////////////////////////////////////////////////////////////////////////// 	
+//修改说明
+//基于正点原子例程的PWM输出实验
+////////////////////////////////////////////////////////////////////////////////// 	 
+
+TIM_HandleTypeDef TIM3_Handler;         //定时器句柄 
+TIM_OC_InitTypeDef TIM3_CH1Handler;     //! 定时器3通道1句柄
+
+//TIM3 PWM部分初始化 
+//PWM输出初始化
+//arr：自动重装值
+//psc：时钟预分频数
+void TIM3_PWM_Init(u16 arr,u16 psc)
+{ 
+    TIM3_Handler.Instance=TIM3;            //定时器3
+    TIM3_Handler.Init.Prescaler=psc;       //定时器分频
+    TIM3_Handler.Init.CounterMode=TIM_COUNTERMODE_UP;//向上计数模式
+    TIM3_Handler.Init.Period=arr;          //自动重装载值
+    TIM3_Handler.Init.ClockDivision=TIM_CLOCKDIVISION_DIV1;
+    HAL_TIM_PWM_Init(&TIM3_Handler);       //初始化PWM
+    
+    TIM3_CH1Handler.OCMode=TIM_OCMODE_PWM1; //! 模式选择PWM1, 使用PWM模式，CCMR寄存器的OC4M位必须为0110或0111（极性不同）
+    TIM3_CH1Handler.Pulse=arr/2;            //设置比较值,此值用来确定占空比，
+                                            //默认比较值为自动重装载值的一半,即占空比为50%
+    TIM3_CH1Handler.OCPolarity=TIM_OCPOLARITY_LOW; //输出比较极性为低 
+    HAL_TIM_PWM_ConfigChannel(&TIM3_Handler,&TIM3_CH1Handler,TIM_CHANNEL_1);//! 配置TIM3通道1, 使用PA6输出
+    
+}
+
+//定时器底层驱动，时钟使能，引脚配置
+//此函数会被HAL_TIM_PWM_Init()调用
+//htim:定时器句柄
+void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *htim)
+{
+    GPIO_InitTypeDef GPIO_Initure;
+	__HAL_RCC_TIM3_CLK_ENABLE();			//使能定时器3
+    __HAL_RCC_GPIOB_CLK_ENABLE();			//开启GPIOB时钟
+	
+    GPIO_Initure.Pin=GPIO_PIN_6;           	//! PA6
+    GPIO_Initure.Mode=GPIO_MODE_AF_PP;  	//复用推挽输出
+    GPIO_Initure.Pull=GPIO_PULLUP;          //上拉
+    GPIO_Initure.Speed=GPIO_SPEED_HIGH;     //高速
+	GPIO_Initure.Alternate=GPIO_AF2_TIM3;	//! PA6复用为TIM3_CH1
+    HAL_GPIO_Init(GPIOA,&GPIO_Initure);     //! GPIOA初始化
+}
+
+//设置TIM通道4的占空比
+//compare:比较值
+void TIM_SetTIM3Compare4(u32 compare)
+{
+	TIM3->CCR1=compare;     //! CCR1对应通道1的捕获/比较寄存器
+}
